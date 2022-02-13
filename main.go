909c33239334c992630cdecde153e250b36f7283
jaychenfe <chenjiehwy@163.com>
2022-02-13 20:33:38 +0800

    06-middleware

--- main.go
+++ main.go
@@ -5,86 +5,38 @@ import (
 )
 
 func main() {
-	router := gin.Default()
-	// 分组 v1
-	v1 := router.Group("/v1")
+	//默认使用了日志中间件和 Recovery 中间件
+	//	r := gin.Default()
+
+	// 创建空白路由
+	r := gin.New()
+
+	// 集成日志中间件
+	// 默认 gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	// 集成 Recovery 中间件
+	r.Use(gin.Recovery())
+
+	// Per route middleware, you can add as many as you desire.
+	r.GET("/benchmark", MyBenchLogger(), benchEndpoint)
+
+	// 认证分组
+	// authorized := r.Group("/", AuthRequired())
+	// 上面的书写方法等价于下面的编码方式
+	authorized := r.Group("/")
+	authorized.Use(AuthRequired())
 	{
-		v1.POST("/login", loginEndpoint)
-		v1.POST("/submit", submitEndpoint)
-		v1.POST("/send", sendEndpoint)
-		v1.POST("/aaa", aaaaEndpoint)
-		v1.POST("/bbb", bbbbEndpoint)
-		v1.POST("/ccc", ccccEndpoint)
-		v1.POST("/ddd", ddddEndpoint)
+		authorized.POST("/login", loginEndpoint)
+		authorized.POST("/submit", submitEndpoint)
+		authorized.POST("/read", readEndpoint)
 	}
 
-	// 分组 v2
-	v2 := router.Group("/v2")
-	{
-		v2.POST("/login", loginEndpoint2)
-		v2.POST("/submit", submitEndpoint2)
-		v2.POST("/send", sendEndpoint2)
-		v2.POST("/aaa", aaaaEndpoint2)
-		v2.POST("/bbb", bbbbEndpoint2)
-		v2.POST("/ccc", ccccEndpoint2)
-		v2.POST("/ddd", ddddEndpoint2)
-
-	}
-	v2Path := v2.Group("/path")
-	v2Path.POST("/aaa", ddddEndpoint3)
-
-	router.Run(":8080")
+	// 启动服务监听 8080 端口
+	r.Run(":8080")
 }
 
-func ddddEndpoint3(context *gin.Context) {
-
-}
-
-func ddddEndpoint2(context *gin.Context) {
-
-}
-
-func ccccEndpoint2(context *gin.Context) {
-
-}
-
-func bbbbEndpoint2(context *gin.Context) {
-
-}
-
-func aaaaEndpoint2(context *gin.Context) {
-
-}
-
-func sendEndpoint2(context *gin.Context) {
-
-}
-
-func submitEndpoint2(context *gin.Context) {
-
-}
-
-func loginEndpoint2(context *gin.Context) {
-
-}
-
-func ddddEndpoint(context *gin.Context) {
-
-}
-
-func ccccEndpoint(context *gin.Context) {
-
-}
-
-func bbbbEndpoint(context *gin.Context) {
-
-}
-
-func aaaaEndpoint(context *gin.Context) {
-
-}
-
-func sendEndpoint(context *gin.Context) {
+func readEndpoint(context *gin.Context) {
 
 }
 
@@ -94,4 +46,17 @@ func submitEndpoint(context *gin.Context) {
 
 func loginEndpoint(context *gin.Context) {
 
+}
+
+func AuthRequired() gin.HandlerFunc {
+
+	return nil
+}
+
+func benchEndpoint(context *gin.Context) {
+
+}
+
+func MyBenchLogger() gin.HandlerFunc {
+	return nil
 }
