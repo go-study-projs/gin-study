92a54836c9deb37ad79a7b8b3146a3ebf2567be3
jaychenfe <chenjiehwy@163.com>
2022-02-13 20:55:45 +0800

    08-model-binding

--- main.go
+++ main.go
@@ -1,34 +1,79 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"time"
+	"net/http"
 )
 
+// Login Binding from JSON
+type Login struct {
+	User     string `form:"user" json:"user" xml:"user"  binding:"required"`
+	Password string `form:"password" json:"password" xml:"password" binding:"required"`
+}
+
 func main() {
-	router := gin.New()
-	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+	router := gin.Default()
 
-		// 自定义日志输出格式
-		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
-			param.ClientIP,
-			param.TimeStamp.Format(time.RFC3339Nano),
-			param.Method,
-			param.Path,
-			param.Request.Proto,
-			param.StatusCode,
-			param.Latency,
-			param.Request.UserAgent(),
-			param.ErrorMessage,
-		)
-	}))
-	// 应用崩溃恢复中间件
-	router.Use(gin.Recovery())
+	// 绑定 JSON 类型参数 ({"user": "manu", "password": "123"})
+	router.POST("/loginJSON", func(c *gin.Context) {
+		var json Login
+		if err := c.ShouldBindJSON(&json); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		if json.User != "manu" || json.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
 	})
 
+	// Example for binding XML (
+	//  <?xml version="1.0" encoding="UTF-8"?>
+	//  <root>
+	//    <user>user</user>
+	//    <password>123</password>
+	//  </root>)
+	router.POST("/loginXML", func(c *gin.Context) {
+		var xml Login
+		if err := c.ShouldBindXML(&xml); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if xml.User != "manu" || xml.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	// 绑定 HTML 形式的参数 (user=manu&password=123)
+	router.POST("/loginForm", func(c *gin.Context) {
+		var form Login
+		// This will infer what binder to use depending on the content-type header.
+		if err := c.ShouldBind(&form); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		if form.User != "manu" || form.Password != "123" {
+			c.JSON(http.StatusUnauthorized, gin.H{"status": "unauthorized"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+
+	// 启动服务并监听 8080 端口
 	router.Run(":8080")
 }
+
+//此外，在进行参数模型绑定时，Gin 还提供了两组绑定方法 MustBindWith 和 ShouldBindWith，
+//一种是必须绑定模式，一种是应该绑定模式。
+//前者在发生解析错误时，会直接触发错误返回，后者需要开发者自己进行错误处理。
+//相关的方法如下所示：必须绑定模式有 Bind,  BindJSON,  BindXML,  BindQuery,  BindYAML，
+//应该绑定模式有 ShouldBind,  ShouldBindJSON,  ShouldBindXML,  ShouldBindQuery,  ShouldBindYAML。
