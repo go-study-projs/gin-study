e9fc2b835ce322cfe8b24b4fe6157fc79b360cf2
jaychenfe <chenjiehwy@163.com>
2022-02-13 20:37:51 +0800

    07-log-diy

--- main.go
+++ main.go
@@ -2,61 +2,25 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"io"
+	"os"
 )
 
 func main() {
-	//默认使用了日志中间件和 Recovery 中间件
-	//	r := gin.Default()
+	// 不启动颜色控制
+	gin.DisableConsoleColor()
 
-	// 创建空白路由
-	r := gin.New()
+	// 创建日志文件
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f)
 
-	// 集成日志中间件
-	// 默认 gin.DefaultWriter = os.Stdout
-	r.Use(gin.Logger())
+	// 使用如下代码，可以实现日志文件记录和 console 同步显示
+	// gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
 
-	// 集成 Recovery 中间件
-	r.Use(gin.Recovery())
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
 
-	// Per route middleware, you can add as many as you desire.
-	r.GET("/benchmark", MyBenchLogger(), benchEndpoint)
-
-	// 认证分组
-	// authorized := r.Group("/", AuthRequired())
-	// 上面的书写方法等价于下面的编码方式
-	authorized := r.Group("/")
-	authorized.Use(AuthRequired())
-	{
-		authorized.POST("/login", loginEndpoint)
-		authorized.POST("/submit", submitEndpoint)
-		authorized.POST("/read", readEndpoint)
-	}
-
-	// 启动服务监听 8080 端口
-	r.Run(":8080")
-}
-
-func readEndpoint(context *gin.Context) {
-
-}
-
-func submitEndpoint(context *gin.Context) {
-
-}
-
-func loginEndpoint(context *gin.Context) {
-
-}
-
-func AuthRequired() gin.HandlerFunc {
-
-	return nil
-}
-
-func benchEndpoint(context *gin.Context) {
-
-}
-
-func MyBenchLogger() gin.HandlerFunc {
-	return nil
+	router.Run(":8080")
 }
